577fc23f9d148c99f0bf8d8e29cbf1b4cd5eda5e
Anup Bagali <anup.bagali1@gmail.com>
2019-12-31 21:43:13 -0500

    add more functions to submodule

--- include/Cfs/Submodule.hpp
+++ include/Cfs/Submodule.hpp
@@ -12,15 +12,23 @@ class Submodule{
             this->name = name;
             log("INFO: Creating Submodule: " + name);
         }
-        void start(){
-            log("Starting submodule: " + this->name);
-        }
-        void set_modules(map<string, Submodule*> dependencies){
+        void setModules(map<string, Submodule*> dependencies){
             this->modules = dependencies;
         }
-        bool has_module(string name){
+        bool hasModule(string name){
             return this->modules.count(name);
         }
+        Submodule* getModuleOrRaiseError(string moduleName){
+            if(this->hasModule(moduleName)){
+                return this->modules.at(moduleName);
+            }else{
+                log("ERROR: " + this->name + " cannot find " + moduleName);
+            }
+        };
+        virtual void start()=0;
+        virtual void enterNormalMode(){};
+        virtual void enterLowPowerMode(){};
+
 
     private:
         string name;
